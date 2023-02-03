6f9f83ea21969ea3df9ea0b4028cc61ef570472b
taktakgyun <vbmrkk@naver.com>
2023-02-03 15:55:49 +0900

    Refactory handle_connection funtion

--- src/main.rs
+++ src/main.rs
@@ -20,33 +20,28 @@ fn handle_connection(mut stream: TcpStream) {
     stream.read(&mut buffer).unwrap();
 
     let get = b"GET / HTTP/1.1\r\n";
-    if buffer.starts_with(get) {
-        // read html file
-        let file_name = format!("{}/hello.html", TEMPLATES);
-        let mut file = File::open(file_name).unwrap();
 
-        let mut contents = String::new();
-        file.read_to_string(&mut contents).unwrap();
-
-        // set response
-        let response = format!("HTTP/1.1 200 OK\r\n\r\n{}", contents);
-        // response using TCP stream
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
+    let (status_line, file_name) = if buffer.starts_with(get) {
+        (
+            "HTTP/1.1 200 OK\r\n\r\n",
+            format!("{}/hello.html", TEMPLATES),
+        )
     } else {
-        // read html file
-        let file_name = format!("{}/404.html", TEMPLATES);
-        let mut file = File::open(file_name).unwrap();
+        (
+            "HTTP/1.1 404 NOT FOUND\r\n\r\n",
+            format!("{}/404.html", TEMPLATES),
+        )
+    };
+    // read html file
+    let mut file = File::open(file_name).unwrap();
 
-        let mut contents = String::new();
-        file.read_to_string(&mut contents).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
 
-        // set response
-        let status_line = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
-        let response = format!("{}{}", status_line, contents);
+    // set response
+    let response = format!("{}{}", status_line, contents);
 
-        // response using TCP stream
-        stream.write(response.as_bytes()).unwrap();
-        stream.flush().unwrap();
-    }
+    // response using TCP stream
+    stream.write(response.as_bytes()).unwrap();
+    stream.flush().unwrap();
 }
