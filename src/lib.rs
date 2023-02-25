ecfd4c3c95b83fda51804e022ed0021e595859f1
taktakgyun <vbmrkk@naver.com>
2023-02-26 01:27:00 +0900

    Update ThreadPool. Add ThreadPool, Worker struct

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,42 @@
-pub struct ThreadPool;
+use std::thread;
+
+pub struct ThreadPool {
+    workers: Vec<Worker>,
+}
 
 impl ThreadPool {
+    /// Create new ThreadPool
+    /// size is number of thread in pool.
+    ///
+    /// # Panics
+    /// if size == 0, new funtion occur panics
     pub fn new(size: usize) -> ThreadPool {
-        ThreadPool
+        assert!(size > 0);
+
+        let mut workers = Vec::with_capacity(size);
+
+        for id in 0..size {
+            // Create thread and Push thread in Vector
+            workers.push(Worker::new(id))
+        }
+        ThreadPool { workers }
     }
     pub fn execute<F>(&self, f: F)
     where
         F: FnOnce() + Send + 'static,
     {
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+
+        Worker { id, thread }
     }
 }
