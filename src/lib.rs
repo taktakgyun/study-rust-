6b41e1cb2a0ef4ff37bbe20886c9a0ef3bab628b
taktakgyun <vbmrkk@naver.com>
2023-02-28 21:30:43 +0900

    Change lib.rs, Not complete complie, ThreadPool race condition

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,31 @@
+use std::sync::mpsc;
 use std::thread;
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
+    sender: mpsc::Sender<Job>,
 }
+
+struct Job;
 
 impl ThreadPool {
     /// Create new ThreadPool
     /// size is number of thread in pool.
     ///
     /// # Panics
-    /// if size == 0, new funtion occur panics
+    /// if size == 0, new function occur panics
     pub fn new(size: usize) -> ThreadPool {
         assert!(size > 0);
+
+        let (sender, receiver) = mpsc::channel();
 
         let mut workers = Vec::with_capacity(size);
 
         for id in 0..size {
             // Create thread and Push thread in Vector
-            workers.push(Worker::new(id))
+            workers.push(Worker::new(id, receiver));
         }
-        ThreadPool { workers }
+        ThreadPool { workers, sender }
     }
     pub fn execute<F>(&self, f: F)
     where
@@ -34,8 +40,10 @@ struct Worker {
 }
 
 impl Worker {
-    fn new(id: usize) -> Worker {
-        let thread = thread::spawn(|| {});
+    fn new(id: usize, receiver: mpsc::Receiver<Job>) -> Worker {
+        let thread = thread::spawn(|| {
+            receiver;
+        });
 
         Worker { id, thread }
     }
